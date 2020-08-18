c6c2521abfbdd5e4bb301ba601d1f03b8cdab208
romapyshka <33147170+romapyshka@users.noreply.github.com>
2020-08-18 13:32:57 +0300

    created

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 80;
+
+app.get('/', (req,res) => {
+    res.end('<h1>Welcome User!</h1>')
+});
+
+
+app.listen(PORT, () => {
+    console.log('Server start')
+});
