0c492bc1e72570024af745f7ec0ab7f079c94cf1
romapyshka <33147170+romapyshka@users.noreply.github.com>
2020-08-18 16:26:58 +0300

    Change index.js to test git

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 80;
 
 app.get('/', (req,res) => {
-    res.end('<h1>Welcome User!</h1>')
+    res.end('<h1>Welcome User! Wait...</h1>')
 });
 
 
