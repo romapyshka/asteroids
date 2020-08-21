a530c9b7a1392a562394fa6d2cb5632e61c3e688
romapyshka <33147170+romapyshka@users.noreply.github.com>
2020-08-21 19:57:22 +0300

    Three.js first try

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,9 @@
-"use strict";
+import express from 'express';
+const app = express();
+const path = require('path');
+app.use("/dist", express.static('./dist/'));
+app.get('/', function (req, res) {
+    res.sendFile(path.resolve('./index.html'));
+});
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log('Server running'));
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express, { Application, Request, Response, NextFunction} from 'express';
 
 const app: Application = express();
+const path = require('path');
 
-app.get('/', (req: Request, res:Response, next: NextFunction) => {
-    res.send("Hello world!");
+app.use("/dist", express.static('./dist/'));
 
+app.get('/', function(req, res) {
+    res.sendFile(path.resolve('./index.html'))
 });
 
-const port = process.env.PORT || 80
-
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log('Server running'));
+
