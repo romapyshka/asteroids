477dd059e519dbae7a195b1107fc95fdb999fc6f
romapyshka <33147170+romapyshka@users.noreply.github.com>
2020-08-31 14:35:07 +0300

    TS Keyboard Controls

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,6 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = __importDefault(require("express"));
+import express from "express";
+
 // integration with heroku
-<<<<<<< HEAD:src/app.ts
 const port = process.env.PORT || 9000;
 const app = express();
 
@@ -15,12 +10,6 @@ app.listen(port, () => console.log('Server running'));
 
 
 
-=======
-var port = process.env.PORT || 8000;
-var app = express_1.default();
-app.use(express_1.default.static("public"));
-app.listen(port, function () { return console.log('Server running'); });
->>>>>>> 385298475da6d461f6556da544a9d117c7d687fb:app.js
 // import * as path from "path";
 // import express, { Application, Request, Response, NextFunction} from 'express';
 //
